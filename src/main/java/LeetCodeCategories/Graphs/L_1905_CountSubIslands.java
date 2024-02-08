c1362246a73eb942d0a0acbf9b35e86932bfe2c9
lyagoubisaad <lyagoubisaad@gmail.com>
2024-02-08 02:11:27 +0100

    wip

--- src/main/java/LeetCodeCategories/Graphs/L_1905_CountSubIslands.java
+++ src/main/java/LeetCodeCategories/Graphs/L_1905_CountSubIslands.java
@@ -2,6 +2,7 @@ package LeetCodeCategories.Graphs;
 
 public class L_1905_CountSubIslands {
     int cpt;
+
     public int countSubIslands(int[][] grid1, int[][] grid2) {
         cpt = 0;
         for (int i = 0; i < grid1.length; i++) {
@@ -11,15 +12,16 @@ public class L_1905_CountSubIslands {
         }
         return cpt;
     }
+
     private boolean dfs(int[][] grid1, int[][] grid2, int row, int col) {
         if (row < 0 || col < 0 || row == grid1.length || col == grid1[0].length) return true;
-        if(grid2[row][col] == 0) return true;
+        if (grid2[row][col] == 0) return true;
         if (grid1[row][col] == 0) return false;
         grid2[row][col] = 0;
-        boolean down = dfs(grid1, grid2, row + 1, col);
-        boolean up = dfs(grid1, grid2, row - 1, col);
-        boolean right = dfs(grid1, grid2, row, col + 1);
+        boolean up = dfs(grid1, grid2, row + 1, col);
+        boolean down = dfs(grid1, grid2, row - 1, col);
         boolean left = dfs(grid1, grid2, row, col - 1);
-        return down && up && right && left;
+        boolean right = dfs(grid1, grid2, row, col + 1);
+        return up && down && left && right;
     }
 }
