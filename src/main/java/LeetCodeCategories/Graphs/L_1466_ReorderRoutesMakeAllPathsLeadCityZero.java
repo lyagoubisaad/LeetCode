32c6687c820a13f2df1673cd402f030f7d04b925
lyagoubisaad <lyagoubisaad@gmail.com>
2024-02-21 00:03:49 +0100

    wip

---
+++ src/main/java/LeetCodeCategories/Graphs/L_1466_ReorderRoutesMakeAllPathsLeadCityZero.java
@@ -0,0 +1,30 @@
+package LeetCodeCategories.Graphs;
+
+import java.util.*;
+
+public class L_1466_ReorderRoutesMakeAllPathsLeadCityZero {
+    int cpt = 0;
+    HashSet<String> oldGraphs = new HashSet<>();
+    HashSet<Integer> seen = new HashSet<>();
+    HashMap<Integer, List<Integer>> relations = new HashMap<>();
+    public int minReorder(int n, int[][] connections) {
+        for (int[] connection: connections) {
+            int a = connection[0];
+            int b = connection[1];
+            oldGraphs.add(a + "->" + b);
+            relations.computeIfAbsent(a, val -> new ArrayList<>()).add(b);
+            relations.computeIfAbsent(b, val -> new ArrayList<>()).add(a);
+        }
+        dfs(0, -1);
+        return cpt;
+    }
+
+    private void dfs(int curr, int idx) {
+        if(seen.contains(curr)) return;
+        seen.add(curr);
+        if(oldGraphs.contains(idx + "->" + curr)) cpt++;
+        for (int elem: relations.get(curr)) {
+            dfs(elem, curr);
+        }
+    }
+}
---
+++ src/test/java/LeetCodeCategories/Graphs/L_1466_ReorderRoutesMakeAllPathsLeadCityZeroTest.java
@@ -0,0 +1,29 @@
+package LeetCodeCategories.Graphs;
+
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class L_1466_ReorderRoutesMakeAllPathsLeadCityZeroTest {
+
+    L_1466_ReorderRoutesMakeAllPathsLeadCityZero myClass = new L_1466_ReorderRoutesMakeAllPathsLeadCityZero();
+
+    @Test
+    public void firstTest() {
+        int[][] connections = new int[][] {{0,1},{1,3},{2,3},{4,0},{4,5}};
+        assertEquals(3, myClass.minReorder(6, connections));
+    }
+
+    @Test
+    public void secondTest() {
+        int[][] connections = new int[][] {{1,0},{1,2},{3,2},{3,4}};
+        assertEquals(2, myClass.minReorder(5, connections));
+    }
+
+    @Test
+    public void thirdTest() {
+        int[][] connections = new int[][] {{1,0},{2,0}};
+        assertEquals(0, myClass.minReorder(3, connections));
+    }
+
+}
