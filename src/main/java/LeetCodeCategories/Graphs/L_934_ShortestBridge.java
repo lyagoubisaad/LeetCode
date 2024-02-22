1e8d9943ca2bc01175679ef32390a021e1d0f146
lyagoubisaad <lyagoubisaad@gmail.com>
2024-02-22 22:56:48 +0100

    wip

---
+++ src/main/java/LeetCodeCategories/Graphs/L_934_ShortestBridge.java
@@ -0,0 +1,7 @@
+package LeetCodeCategories.Graphs;
+
+public class L_934_ShortestBridge {
+    public int shortestBridge(int[][] grid) {
+        return 0;
+    }
+}
---
+++ src/test/java/LeetCodeCategories/Graphs/L_1958_CheckifMoveisLegalTest.java
@@ -0,0 +1,29 @@
+package LeetCodeCategories.Graphs;
+
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class L_1958_CheckifMoveisLegalTest {
+    L_1958_CheckifMoveisLegal myClass = new L_1958_CheckifMoveisLegal();
+
+    @Test
+    public void firstTest() {
+        char[][] board = new char[][] {
+                {'.','.','.','B','.','.','.','.'},{'.','.','.','W','.','.','.','.'},{'.','.','.','W','.','.','.','.'},
+                {'.','.','.','W','.','.','.','.'},{'W','B','B','.','W','W','W','B'},{'.','.','.','B','.','.','.','.'},
+                {'.','.','.','B','.','.','.','.'},{'.','.','.','W','.','.','.','.'}
+        };
+        assertEquals(true, myClass.checkMove(board, 4, 3, 'B'));
+    }
+
+    @Test
+    public void secondTest() {
+        char[][] board = new char[][] {
+                {'.','.','.','.','.','.','.','.'},{'.','B','.','.','W','.','.','.'},{'.','.','W','.','.','.','.','.'},
+                {'.','.','.','W','B','.','.','.'},{'.','.','.','.','.','.','.','.'},{'.','.','.','.','B','W','.','.'},
+                {'.','.','.','.','.','.','W','.'},{'.','.','.','.','.','.','.','B'}
+        };
+        assertEquals(false, myClass.checkMove(board, 4, 4, 'W'));
+    }
+}
