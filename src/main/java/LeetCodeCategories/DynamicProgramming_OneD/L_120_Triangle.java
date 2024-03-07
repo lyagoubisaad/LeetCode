c42b8f28baa72afd17c467f46a42b49f26c23729
lyagoubisaad <lyagoubisaad@gmail.com>
2024-03-07 01:08:45 +0100

    wip

--- src/main/java/LeetCodeCategories/DynamicProgramming_OneD/L_120_Triangle.java
+++ src/main/java/LeetCodeCategories/DynamicProgramming_OneD/L_120_Triangle.java
@@ -1,32 +1,21 @@
 package LeetCodeCategories.DynamicProgramming_OneD;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class L_120_Triangle {
-
-    int min = Integer.MAX_VALUE;
-
     public int minimumTotal(List<List<Integer>> triangle) {
-        Map<List<Integer>, Integer> map = new HashMap<>();
-        map.put(List.of(-1,0), 0);
-        helper(triangle, 0, 0, map);
-        return min;
-    }
-
-    private void helper(List<List<Integer>> triangle, int floor, int idx, Map<List<Integer>, Integer> map) {
-        if(map.containsKey(List.of(floor, idx))) return;
-        if(idx >= triangle.get(floor).size()) return;
-        int curr = triangle.get(floor).get(idx);
-        int prvHor = map.getOrDefault(List.of(floor-1, idx), Integer.MAX_VALUE);
-        int prvVer = map.getOrDefault(List.of(floor-1, idx-1), Integer.MAX_VALUE);
-        map.put(List.of(floor, idx), curr+Math.min(prvVer, prvHor));
-        if(floor == triangle.size()-1) {
-            min = Math.min(min, map.get(List.of(floor, idx)));
-            return;
+        int[] memo = new int[triangle.size()];
+        List<Integer> lastRow = triangle.get(triangle.size() - 1);
+        for (int i = 0; i < lastRow.size(); i++) {
+            memo[i] = lastRow.get(i);
         }
-        helper(triangle, floor+1, idx, map);
-        helper(triangle, floor+1, idx+1, map);
+        for (int row = triangle.size() - 2; row >= 0; row--) {
+            List<Integer> currentRow = triangle.get(row);
+            for (int col = 0; col < currentRow.size(); col++) {
+                int value = currentRow.get(col);
+                memo[col] = value + Math.min(memo[col], memo[col + 1]);
+            }
+        }
+        return memo[0];
     }
 }
