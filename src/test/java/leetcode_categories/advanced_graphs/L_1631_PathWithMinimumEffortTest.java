a8e7b9648db4d6d45c466167b33fb3d1b432c39a
Lyagoubi Saad <lyagoubisaad@gmail.com>
2024-03-30 01:59:28 +0100

    [AdvancedGraphs]: 30 March - Path with minimum effort

--- src/test/java/leetcode_categories/advanced_graphs/L_1631_PathWithMinimumEffortTest.java
+++ src/test/java/leetcode_categories/advanced_graphs/L_1631_PathWithMinimumEffortTest.java
@@ -21,11 +21,11 @@ public class L_1631_PathWithMinimumEffortTest {
     @Test
     @Disabled("This method is not a test, just a utility method")
     void convert() {
-        convertor.convert("");
+        convertor.convert("[[4,3,4,10,5,5,9,2],[10,8,2,10,9,7,5,6],[5,8,10,10,10,7,4,2],[5,1,3,1,1,3,1,9],[6,4,10,6,10,9,4,6]]");
     }
 
     @Test
-    void firstTest() {
+    void testFirst() {
         //Given
         int[][] heights = new int[][] {{1,2,2},{3,8,2},{5,3,5}};
         //When
@@ -35,7 +35,7 @@ public class L_1631_PathWithMinimumEffortTest {
     }
 
     @Test
-    public void TestTwo() {
+    public void testTwo() {
         //Given
         int[][] heights = new int[][] {{1,2,3},{3,8,4},{5,3,5}};
         //When
@@ -45,12 +45,32 @@ public class L_1631_PathWithMinimumEffortTest {
     }
 
     @Test
-    public void TestThree() {
+    public void testThree() {
         //Given
         int[][] heights = new int[][] {{1,2,1,1,1},{1,2,1,2,1},{1,2,1,2,1},{1,2,1,2,1},{1,1,1,2,1}};
         //When
         int result = myClass.minimumEffortPath(heights);
         //Then
         assertEquals(0, result);
+    }
+
+    @Test
+    public void tesTForth() {
+        //Given
+        int[][] heights = new int[][] {{3}};
+        //When
+        int result = myClass.minimumEffortPath(heights);
+        //Then
+        assertEquals(0, result);
+    }
+
+    @Test
+    public void tesTFifth() {
+        //Given
+        int[][] heights = new int[][] {{4,3,4,10,5,5,9,2},{10,8,2,10,9,7,5,6},{5,8,10,10,10,7,4,2},{5,1,3,1,1,3,1,9},{6,4,10,6,10,9,4,6}};
+        //When
+        int result = myClass.minimumEffortPath(heights);
+        //Then
+        assertEquals(5, result);
     }
 }
