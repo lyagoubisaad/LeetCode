05ab3176e0b937f5ca5ecfacaa4dcca0f4b308c8
lyagoubisaad <lyagoubisaad@gmail.com>
2024-04-07 20:47:23 +0200

    [Arrays & Hashing]: 07 April - Start the topic

---
+++ src/main/java/leetcode_categories/string/L49GroupAnagrams.java
@@ -0,0 +1,14 @@
+package leetcode_categories.string;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import static leetcode_categories.string.L_242_Valid_Anagram.isAnagram;
+
+public class L49GroupAnagrams {
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        return new ArrayList<>();
+    }
+
+}
